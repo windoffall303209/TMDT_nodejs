00094dae55ff07654f2b898e42a2a4b8e9d33381
WIND OF FALL <nvuthanh4@gmail.com>
2026-05-13 20:42:39 +0700

    dọn file thừa

--- controllers/adminController.js
+++
@@ -1,2 +0,0 @@
-// Giữ tương thích cho test/import cũ, logic admin mới được gom trong controllers/admin.
-module.exports = require('./admin');
--- controllers/chatController.js
+++
@@ -1,2 +0,0 @@
-// Giu tuong thich cho test/import cu, logic chat moi duoc gom trong controllers/chat.
-module.exports = require('./chat');
